681b952e3eb2179008a59e05bcc096809a0ac87b
vibek <vibek.2022ug1094@iiitranchi.ac.in>
2025-04-05 10:23:01 +0530

    setup basic bodyparser

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 const express=require("express");
+const bodyparser=require("body-parser");
 
 const {PORT}=require('./config/serverConfig')
 
 const setupStartServer=async()=>{
 
     const app=express();
+
+    app.use(bodyparser.json());
+    app.use(bodyparser.urlencoded({extended:true}));
+    
 
     app.listen(PORT,()=>{
         console.log(`Server started at ${PORT}`);
