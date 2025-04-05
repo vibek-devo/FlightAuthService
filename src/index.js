9a48bc45e5047b6ccfba69e6561347128058e916
vibek <vibek.2022ug1094@iiitranchi.ac.in>
2025-04-05 09:31:38 +0530

    setup basic dotenv

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const express=require("express");
+
+const {PORT}=require('./config/serverConfig')
+
+const setupStartServer=async()=>{
+
+    const app=express();
+    const PORT=3000;
+
+    app.listen(PORT,()=>{
+        console.log(`Server started at ${PORT}`);
+        // console.log(process.env);
+    });
+}
+
+setupStartServer();
